6261cc4c674320aebbb2d9af09d0ad2cdaa4d53c
DelfinSR <delsanrub@alum.us.es>
2023-05-14 19:06:44 +0200

    some tests added

--- src/test/java/aiss/gitbucketminer/service/ProjectServiceTest.java
+++ src/test/java/aiss/gitbucketminer/service/ProjectServiceTest.java
@@ -35,10 +35,15 @@ class ProjectServiceTest {
     @Test
     void getProject() throws ProjectNotFoundException {
         Project project = projectService.getProject(owner, repo);
-        System.out.println(project.id);
-        System.out.println(project.name);
-        System.out.println(project.webUrl);
-        System.out.println(project.getCommits());
-        System.out.println(project.getIssues());
+        assertEquals(project.id,"0","The id doesn't match");
+        assertEquals(project.name,repo,"The name doesn't match");
+        assertEquals(project.webUrl, "http://localhost:4040/" + owner + "/" + repo,"The url doesn't match");
+        assertNotNull(project.getCommits(), "Commits are null");
+        assertNotNull(project.getIssues(), "Issues are null");
+    }
+
+    @Test
+    void getProjectThatDoesntExist() throws ProjectNotFoundException {
+        Project project = projectService.getProject(repo, repo);
     }
 }
